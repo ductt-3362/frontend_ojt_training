c323f21c8da7a38966c47c69a8b38dec149ed5f0
tran thang duc <tran.thang.duc@sun-asterisk.com>
2023-09-21 09:18:28 +0700

    fix: comment PR

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,4 +1,4 @@
 import { api } from "./axios";
-export const getAllCategoriesApi = async () => await api.get("/categories");
-export const getDetailProductApi = async (id) =>
-  await api.get(`/books/${id}?_expand=category`);
+export const getAllCategoriesApi = () => api.get("/categories");
+export const getDetailProductApi = (id) =>
+  api.get(`/books/${id}?_expand=category`);
