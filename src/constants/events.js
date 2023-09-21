49f9e7c36f0775f073a75110cfd58648bebc1ec0
tran thang duc <tran.thang.duc@sun-asterisk.com>
2023-09-21 10:35:02 +0700

    feat: sort doing

--- src/constants/events.js
+++ src/constants/events.js
@@ -2,3 +2,4 @@ export const INPUT_EVENT = "input";
 export const CHANGE_EVENT = "change";
 export const MODEL_VALUE = "modelValue";
 export const UPDATE_MODEL_VALUE = "update:modelValue";
+export const UPDATE_SELECT = "update:select";
