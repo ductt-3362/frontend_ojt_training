847e930998b68a04c5bb8add35cdb7cb3af407d1
Do Duy Anh <do.duy.anh@sun-asterisk.com>
2023-09-29 13:39:50 +0700

    feat: sort in search page

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { getBooksByKeywordApi } from "@apis/book";
 import BookList from "@components/BookList.vue";
+import SelectSort from "@components/SelectSort.vue";
+import { productApiMessage } from "@locales/vi/messages";
+import { useToast } from "vue-toast-notification";
 import { onMounted, ref, watch } from "vue";
 
+const $toast = useToast();
 const props = defineProps(["query"]);
 const books = ref([]);
 
@@ -13,8 +17,17 @@ const handleSearch = async () => {
       const { data } = await getBooksByKeywordApi(props.query);
       books.value = data;
     } catch (error) {
-      // handle error
+      $toast.error(productApiMessage.error);
     }
+};
+
+const fetchBooksBySortParams = async (params) => {
+  try {
+    const { data } = await getBooksByKeywordApi(props.query, params);
+    books.value = data;
+  } catch (error) {
+    $toast.error(productApiMessage.error);
+  }
 };
 
 watch(() => props.query, handleSearch);
@@ -24,9 +37,12 @@ onMounted(handleSearch);
 
 <template>
   <div class="my-12">
-    <p v-if="query" class="text-3xl">
-      Kết quả: Có {{ books.length }} sản phẩm chứa từ khóa "{{ query }}"
-    </p>
+    <div v-if="query" class="flex justify-between">
+      <p class="text-3xl">
+        Kết quả: Có {{ books.length }} sản phẩm chứa từ khóa "{{ query }}"
+      </p>
+      <SelectSort @handle-sort="(params) => fetchBooksBySortParams(params)" />
+    </div>
     <p v-else class="text-3xl">Hãy nhập từ khóa để tìm kiếm</p>
     <BookList :list="books" />
   </div>
