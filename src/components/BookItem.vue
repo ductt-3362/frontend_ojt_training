94abbeb1987f4966d3d08a3bf4ad35be3347e51c
Do Duy Anh <do.duy.anh@sun-asterisk.com>
2023-10-12 16:41:02 +0700

    fix: carousel and improve book item ui

--- src/components/BookItem.vue
+++ src/components/BookItem.vue
@@ -7,15 +7,17 @@ defineProps(["book"]);
 const productStore = useProductStore();
 
 const style = reactive({
-  name: "transtion-colors line-clamp-2 text-md tracking-tight text-gray-900 duration-150 hover:text-red-600 h-12",
+  name: "line-clamp-2 text-md tracking-tight text-gray-900 h-12",
   price: "text-lg font-bold text-red-700",
   image:
     "max:lg:h-60 h-80 w-full rounded-t-lg object-cover max-xl:h-72 max-md:h-80 max-sm:h-60",
   container:
     "relative h-fit rounded-lg border border-gray-200  bg-white shadow-md transition-transform duration-300 hover:scale-105 hover:transform",
   badge:
-    "absolute right-2 top-2 mr-2 rounded bg-red-100 px-2.5 py-0.5 text-sm font-medium text-red-800",
+    "absolute right-2 top-2 rounded bg-red-100 px-2.5 py-0.5 text-sm font-medium text-red-800",
   oldPrice: "text-gray-500 line-through",
+  layer:
+    "absolute left-0 top-0 z-10 h-full w-full rounded-lg bg-black/60 p-5 opacity-0 transition-opacity duration-300 hover:opacity-100 text-white",
 });
 
 const handleAddSeenProduct = (product) => {
@@ -26,25 +28,23 @@ const handleAddSeenProduct = (product) => {
 <template>
   <div :class="style.container">
     <span :class="style.badge">-10%</span>
-    <router-link
-      :to="{ path: `/books/${book.slug}` }"
-      @click="handleAddSeenProduct(book)"
-    >
-      <img :class="style.image" :src="book.image" alt="product image" />
-    </router-link>
+    <img :class="style.image" :src="book.image" alt="product image" />
     <div class="p-4">
-      <router-link
-        :to="{ path: `/books/${book.slug}` }"
-        @click="handleAddSeenProduct(book)"
-      >
-        <h5 :class="style.name">
-          {{ book.name }}
-        </h5>
-      </router-link>
+      <h5 :class="style.name">{{ book.name }}</h5>
       <div class="flex justify-between">
         <div :class="style.price">{{ formatPrice(book.price) }}</div>
         <div :class="style.oldPrice">{{ formatPrice(book.price * 1.1) }}</div>
       </div>
     </div>
+    <router-link
+      :to="{ path: `/books/${book.slug}` }"
+      @click="handleAddSeenProduct(book)"
+    >
+      <div :class="style.layer">
+        <p>{{ book.name }}</p>
+        <p class="mb-4">Tác giả: {{ book.author }}</p>
+        <p class="line-clamp-5">Mô tả: {{ book.description }}</p>
+      </div>
+    </router-link>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
     "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
